56d8ee2f0f9e99f58f7c13703a26e78736d3cec4
AudreyLorentzen <aclorentzen@fullsail.edu>
2015-05-09 15:59:35 -0400

    Finished lab 3

--- Day3Lab/js/Lorentzen_Audrey_Day3Lab.js
+++ Day3Lab/js/Lorentzen_Audrey_Day3Lab.js
@@ -4,25 +4,38 @@
 //Depending on your age, you will get to see either or.
 
 //Initial variable
-var yourName = "";
+var yourName = "Audrey";
 var firstMovie = "\"Unfriended\"";
 var secondMovie = "\"Home\"";
-var age = "";
-var confirm = true;
+var age = 21;
+var ofAge = true;
+var alcoholicDrink = "Long island tea";
+var regDrink = "Sweet tea";
 
-//Prompt for the user input
+//Ouput
+console.log("Hi my name is " +yourName+ " and I'm about to go to the movies.");
+console.log("I'm very indecisive and can't choose between "+firstMovie+ " or " +secondMovie+ "." );
+console.log("I don't have to worry about my age since I am " +age+ ".");
+console.log("I like scary movies the best, so I am going to watch " +firstMovie+ ".");
 
-age = prompt("How old are you?");
 
-//Create first conditional
 
-if(age<18)
-{
-    console.log("Since you are under age, you can watch the movie named " +secondMovie+ ".");
+//Confirm
+ofAge = confirm("Hello " +yourName+ "," + " do you like alcoholic beverages??");
+
+if(ofAge===true) {
+    console.log("Go check out the bar " +yourName+ ".");
+    alcoholicDrink=prompt("What would you like to drink then?");
+    console.log("I would like to drink a " +alcoholicDrink+ " please.");
+
 }else
 {
-    console.log("Since you are of age, you can watch the movie named " +firstMovie+ ".")
+    console.log("Enjoy the movie!");
+    regDrink=prompt("Since you don't like alcohol, is there anything else you would like to drink?");
+    console.log("I don't like alcohol so I decided to get myself some " +regDrink+ ".");
 }
+
+
 
 
 
