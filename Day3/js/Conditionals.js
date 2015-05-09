89059fe2834b6d917a655d0d544857146b9f4f2c
AudreyLorentzen <aclorentzen@fullsail.edu>
2015-05-09 10:14:34 -0400

    Conditionals

---
+++ Day3/js/Conditionals.js
@@ -0,0 +1,24 @@
+
+ /*
+Audrey Lorentzen
+5-9-15
+Condtionals & Prompts
+*/
+ //alert("Still testing.");
+
+ //Initial variable
+ var myName ="Audrey Lorentzen";
+ var newName= "";
+
+ //Prompt for the user input
+ newName = prompt("Please enter your name here:");
+
+ //Create our first conditional
+ if(newName === myName)
+ {
+     //This is the code that runs if the conditional is true
+     console.log("Greetings, "+myName+".");
+
+ }
+
+
