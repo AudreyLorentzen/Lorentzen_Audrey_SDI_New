bd390a5bdc4aa4677ac073bf3e9a8c40c86fde56
AudreyLorentzen <aclorentzen@fullsail.edu>
2015-05-09 13:49:34 -0400

    Movies depending of age

--- Day3Lab/js/Lorentzen_Audrey_Day3Lab.js
+++ Day3Lab/js/Lorentzen_Audrey_Day3Lab.js
@@ -1,6 +1,29 @@
 //alert("JavaScript works!");
 
-//Internal storage
-var name = "";
-var
+//What movie are you allowed to go see?
+//Depending on your age, you will get to see either or.
+
+//Initial variable
+var yourName = "";
+var firstMovie = "\"Unfriended\"";
+var secondMovie = "\"Home\"";
+var age = "";
+var confirm = true;
+
+//Prompt for the user input
+
+age = prompt("How old are you?");
+
+//Create first conditional
+
+if(age<18)
+{
+    console.log("Since you are under age, you can watch the movie named " +secondMovie+ ".");
+}else
+{
+    console.log("Since you are of age, you can watch the movie named " +firstMovie+ ".")
+}
+
+
+
 
