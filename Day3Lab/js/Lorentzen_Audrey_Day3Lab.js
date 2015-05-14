281f5759a73fb3a8319577cc2f1573583292fd4a
AudreyLorentzen <aclorentzen@fullsail.edu>
2015-05-14 05:38:45 -0400

    Day 4 Lab Still in process

--- Day3Lab/js/Lorentzen_Audrey_Day3Lab.js
+++ Day3Lab/js/Lorentzen_Audrey_Day3Lab.js
@@ -1,3 +1,12 @@
+/*
+Audrey Lorentzen
+5-9-15
+Lab 3
+ */
+
+
+
+
 //alert("JavaScript works!");
 
 //What movie are you allowed to go see?
@@ -27,12 +36,23 @@ if(ofAge===true) {
     console.log("Go check out the bar " +yourName+ ".");
     alcoholicDrink=prompt("What would you like to drink then?");
     console.log("I would like to drink a " +alcoholicDrink+ " please.");
+    if(alcoholicDrink==="long island tea")
+    {
+        console.log("Prepared to get wasted!");
+    }else {
+        console.log("Sorry, we don't carry that drink.");
+    }
 
 }else
 {
     console.log("Enjoy the movie!");
-    regDrink=prompt("Since you don't like alcohol, is there anything else you would like to drink?");
-    console.log("I don't like alcohol so I decided to get myself some " +regDrink+ ".");
+    regDrink=parseInt(prompt("Since you don't like alcohol, What size of a soft drink would you like?"));
+    console.log("I don't like alcohol so I decided to get myself some a soft drink.");
+    if(regDrink<=16){
+        console.log("Enjoy your medium drink.");
+    }else {
+      console.log("Enjoy your large drink.")
+    }
 }
 
 
---
+++ Day4Lab/js/Lorentzen_Audrey_Lab4errors.js
@@ -0,0 +1,47 @@
+//NAME:  Audrey Lorentzen
+//DATE:  5/12/15
+//Scalable Data Infrastructures
+//Day 4 Lab
+//Assignment 2
+//Find and fix the errors
+
+
+var myName = "John Doe";
+var myJob  = "\"Cat Wrangler\"";
+var myRate = 7.50;
+var numOfCats = 40;
+var employed = true;
+
+console.log("Hello!  My name is " + myName +".");
+console.log("I'm a " + myJob + ".");
+console.log("My current assignment has me wrangling " + numOfCats + " cats.");
+console.log("So, let's get to work!");
+
+//Confirm
+employed =confirm("Hello, are you employed?");
+
+while (numOfCats < 0) {
+    
+    if (employed = true)
+    {
+
+        console.log("I've wrangled another cat.  Only " + numofcats + " left!");
+        employed = prompt("What was your rate? Mr.Wrangle?");
+        {
+            console.log("So, let's get to work!");
+            numOfCats--;
+        }
+
+
+        if (numOfCats === 5) {
+            console.log("I've been fired!  Someone else will have to wrangle the rest!");
+
+            employed = false;
+
+
+        }
+
+    }
+
+}
+
